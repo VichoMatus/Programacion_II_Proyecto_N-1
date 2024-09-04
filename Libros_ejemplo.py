23b6003646037528238ace77adc5f0ce640a1cfb
eliasCARANZa <eliaspatata10@gmail.com>
2024-09-04 11:59:31 -0400

    error

--- Libros_ejemplo.py
+++
@@ -1,9 +0,0 @@
-class Libro:
-    def __init__(self, nombre, autor, categoria, cantidad):
-        self.nombre = nombre
-        self.autor = autor
-        self.categoria = categoria
-        self.cantidad = cantidad
-
-    def __str__(self):
-        return f"Nombre: {self.nombre}, Autor: {self.autor}, Categoría: {self.categoria}, Cantidad: {self.cantidad}"
